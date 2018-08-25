88f09b8d5abb356f2079cdbfa6cb9414d0e2566e
UvarovOleksandr16031989 <alexuvarov1989@ukr.net>
2018-08-26 00:25:26 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $('.aside__burger').click(function() {
-    $('.aside__burger-item').toggleClass('is-active');
+    $('.aside__burger').toggleClass('is-active');
     $('.aside').toggleClass('is-hidden');
+});
+
+
+var mywindow = $(window);
+var mypos = mywindow.scrollTop();
+mywindow.scroll(function() {
+    if(mywindow.scrollTop() > 2700)
+    {
+        $('.header__inner').addClass('rotate');
+    }
+    else
+    {
+        $('.header__inner').removeClass('rotate');
+    }
+    mypos = mywindow.scrollTop();
 });
